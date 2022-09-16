23f24cbd6cffe1e496d0a6cbb060db7d7cbeac62
Gustavo Henrique de Almeida Batista <112674398+casualmente@users.noreply.github.com>
2022-09-16 04:19:25 -0300

    finish d5

--- www/nlw/esports/web/src/components/Form/Input.tsx
+++ www/nlw/esports/web/src/components/Form/Input.tsx
@@ -8,7 +8,7 @@ export function Input(props:InputProps){
     return(
         <input 
         {...props}
-         className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'/>
+        className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'/>
     )
-   
+
 }
