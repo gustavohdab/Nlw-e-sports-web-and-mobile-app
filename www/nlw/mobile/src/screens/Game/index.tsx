c85369fb131e1dacfd8d7f11db424c41f4d1bfd2
Gustavo Henrique de Almeida Batista <112674398+casualmente@users.noreply.github.com>
2022-09-16 10:20:10 -0300

    finish d5

--- www/nlw/mobile/src/screens/Game/index.tsx
+++ www/nlw/mobile/src/screens/Game/index.tsx
@@ -14,11 +14,12 @@ import { GameParams } from '../../@types/navigation';
 import { DuoCard, DuoCardProps } from '../../components/DuoCard';
 import { Heading } from '../../components/Heading';
 import { Background } from '../../components/Background';
-
+import { DuoMatch } from '../../components/DuoMatch';
 
 
 export function Game() {
   const[duos, setDuos] = useState<DuoCardProps[]>([])
+  const[discordDuoSelected, setDiscordDuoSelected]= useState('');
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -26,6 +27,13 @@ export function Game() {
   
   function handleGoBack(){
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string){
+    fetch(`http://192.168.1.87:3333/ads/${adsId}/discord`)
+    .then(response => response.json())
+    .then(data => setDiscordDuoSelected(data.discord));
+    
   }
 
   useEffect(() => {
@@ -71,7 +79,7 @@ export function Game() {
       renderItem={({item}) => (
         <DuoCard
           data={item}
-          onConnect = {() => {}}
+          onConnect = {() => getDiscordUser(item.id)}
         />
       )}
       horizontal
@@ -83,6 +91,12 @@ export function Game() {
           Não há anúncios publicados ainda.
         </Text>
       )}
+      />
+
+      <DuoMatch
+        visible={discordDuoSelected.length > 0}
+        discord={discordDuoSelected}
+        onClose={() => setDiscordDuoSelected('')}
       />
     </SafeAreaView>
     </Background>
